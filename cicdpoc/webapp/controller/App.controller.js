8afcab7ecd62e2ff9bb7bb7fbbd0ba67e92cb855
Vengadesh R <vrajendran7@slb.com>
2024-03-21 05:22:16 +0000

    test

---
+++ cicdpoc/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("com.cicd.cicdpoc.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
